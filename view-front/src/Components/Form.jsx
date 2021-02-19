ae0c78be164f10347e47cceea90aeca034f70f66
DHANYA KUNHIKRISHNAN <63437424+Dhanya-krishnan2@users.noreply.github.com>
2021-02-19 09:27:40 +0800

    added the changes to by caliing the events

--- view-front/src/Components/Form.jsx
+++ view-front/src/Components/Form.jsx
@@ -8,21 +8,37 @@ export default function Form() {
         phoneNumber: ""
 
     });
+    // this handlefunction is to accept the name entering of a user, we using the spread operator to spread the values
+    const handleFullNameInputChange = (event) => {
+        setValues({...values, fullName: event.target.value})
+    }
+    // setting it for email 
+    const handleEmailInputChange = (event) => {
+        setValues({...values, email: event.target.value})
+    }
+    // setting it for phone number
+    const handlePhoneNumberInputChange = (event) => {
+        setValues({...values, phoneNumber: event.target.value})
+    }
+
     return (
         <div className="form-container">
         <form className="register-form">
         <input className="form-field"
+               onChange={handleFullNameInputChange}
                placeholder="Full-name"
                name="fullName"
                value={values.fullName}
         />
         <input className="form-field"
+             onChange={handleEmailInputChange}
                placeholder="
                Email addres"
                name="email"
                value={values.email}
         />
         <input className="form-field"
+               onChange={handlePhoneNumberInputChange}
                placeholder="
                Phone Number"
                name="phoneNumber"
