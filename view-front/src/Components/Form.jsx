a7d2c7ff812343955e99e338e3bc39abea1529f6
DHANYA KUNHIKRISHNAN <63437424+Dhanya-krishnan2@users.noreply.github.com>
2021-02-19 10:06:03 +0800

    added form validation message

--- view-front/src/Components/Form.jsx
+++ view-front/src/Components/Form.jsx
@@ -8,6 +8,9 @@ export default function Form() {
         phoneNumber: ""
 
     });
+    // here we are declaring a hook to show whether the submitting quote is successful or not
+    // and using unary operator for submission if successful submission giving the message otherwise null
+    const [submitted, setSubmitted] = useState(false);
     // this handlefunction is to accept the name entering of a user, we using the spread operator to spread the values
     const handleFullNameInputChange = (event) => {
         setValues({...values, fullName: event.target.value})
@@ -20,16 +23,32 @@ export default function Form() {
     const handlePhoneNumberInputChange = (event) => {
         setValues({...values, phoneNumber: event.target.value})
     }
+    // for the validation to happen setting up the hook
+      const [valid, setValidation ] = useState(false);
+    // setting up events for submit button
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if(values.fullName && values.email && values.phoneNumber ){
+            setValidation(true);
+        }
+        setSubmitted(true);
+
+    }
 
     return (
         <div className="form-container">
-        <form className="register-form">
+        <form className="register-form" onSubmit={handleSubmit}>
+        {submitted && valid ? <div className="success-message"> Success! Thank you for registering with us</div> :null}
+         
         <input className="form-field"
                onChange={handleFullNameInputChange}
                placeholder="Full-name"
                name="fullName"
                value={values.fullName}
+
         />
+        {submitted &&  !values.fullName ? <span> Please enter you full name</span> : null }
+        
         <input className="form-field"
              onChange={handleEmailInputChange}
                placeholder="
@@ -37,6 +56,8 @@ export default function Form() {
                name="email"
                value={values.email}
         />
+        {submitted && !values.email ? <span> Please enter you email address</span> : null}
+        
         <input className="form-field"
                onChange={handlePhoneNumberInputChange}
                placeholder="
@@ -44,6 +65,8 @@ export default function Form() {
                name="phoneNumber"
                value={values.phoneNumber}
         />
+        { submitted && !values.phoneNumber ?<span> Please enter your phone number</span>: null}
+        
         <button className="form-container" type="submit">
            GET MY FREE QUOTE
         </button>
